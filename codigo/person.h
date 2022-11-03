9afce5a5fc0d03a9477e1664c8a2f30e92f59aeb
Luciano Martinez <lucianoemartinezl@gmail.com>
2022-11-03 12:48:15 -0300

    declaracion de variables privadas

--- codigo/person.h
+++ codigo/person.h
@@ -2,5 +2,13 @@
 #define _PERSON_H_
 #include <iostream>
 #include <cstdio>
+class person{
+private:
+    int ID, Run;
+    char DV, Names[255], FatherName[255],
+    MotherName[255], Password[255];
+    time_t
+public:
 
+};
 #endif
