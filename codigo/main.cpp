c7e025bf2d8bc1ef258e857e8baf6daea04fc8e0
Luciano Martinez <lucianoemartinezl@gmail.com>
2022-11-03 12:26:10 -0300

    Declaracion de objetivos codigo cpp

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -2,3 +2,12 @@
 int main(){
 
 }
+/* Objetivos
+    *Creacion Clase Usuario
+    *Creacion Clase Pacientes
+    *Creacion Clase Preguntas
+    *Creacion Clase Encuesta
+    *Creacion Clase Respuesta
+    *Creacion Clase Examen
+    *
+*/
