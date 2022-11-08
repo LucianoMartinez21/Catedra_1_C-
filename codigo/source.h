2bd2ee893045d286b8f855e96dcc204f99027852
Luciano Martinez <lucianoemartinezl@gmail.com>
2022-11-08 16:23:46 -0300

    Creacion de clases Question y Answer

--- codigo/source.h
+++ codigo/source.h
@@ -14,7 +14,7 @@ public:
 class Person{
 private:
     int ID, Run, Phone;
-    char DV, Names[255], FatherName[255],
+    char DV, Names[255], FatherName[255], // DV = Digito verificador
     MotherName[255], Email[255];
     Birthday Birth;
     enum Genus Gender;
@@ -27,11 +27,14 @@ public:
     bool SetRun(int);
     bool SetPhone(int);
     bool SetDV(char);
-    bool SetNames(char[255]);
-    bool SetFather(char[255]);
-    bool SetMother(char[255]);
+    bool SetFullName(char[255], char[255], char[255]);
+    //bool SetNames(char[255]);
+    //bool SetFather(char[255]);
+    //bool SetMother(char[255]);
     bool SetEmail(char[255]);
-    
+    bool SetBirthday(Birthday);
+    bool SetGender(Genus);
+
     //Getters    
     int GetID();
     int GetRun();
@@ -40,12 +43,12 @@ public:
     char GetNames();
     char GetFather();
     char GetMother();
-    void GetNombreCompleto();
-    //bool SetPassword(char[255]);
-    //char GetPassword();
+    void GetFullName();
+
     //char GetEmail();
     bool Borrar();
     void GetDeleteAt();
+
 };
 
 class User : public Person{
@@ -62,5 +65,28 @@ private:
 public:
     bool SetObservation(char[255]);
     char GetObservation();
+};
+
+class Question{
+private:
+    int ID;
+    char QuestionText[255];
+    time_t DeleteAt;
+public:
+    bool Borrar();
+    void GetDeleteAt();
+};
+
+class Answer{
+private:
+    int ID, Point;
+    char Text[255], Observation[255];
+    time_t DeleteAt;
+public:
+    void AssignedQuestion(Question);
+    bool SetObservation(char[255]);
+    char GetObservation();
+    bool Borrar();
+    void GetDeleteAt();
 };
 #endif
