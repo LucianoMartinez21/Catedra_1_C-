88ac1988c9964bab66451eef77fe20c0c68b37b8
Luciano Martinez <lucianoemartinezl@gmail.com>
2022-11-13 15:50:28 -0300

    Actualizacion main Creador de encuestador

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -4,17 +4,18 @@
 using namespace std;
 //Functions
 void Menu(int);
-
+/*PascalCase*/
 //Global Variables
 fstream UserFile("User/User.txt");
 fstream SurveyFile("Surveyed/Surveyed.txt");
 fstream TestFile("Test/Test.txt");
 fstream QuestionFile("Question/Question.txt");
 fstream AnswerFile("Answer/Answer.txt");
-string mainDir("/Catedra_1_C-");
-int Option;
-
+string MainDir("/Catedra_1_C-");
+int Option, IdCouter;
+void IdVerifier();
 int main(){
+    IdVerifier();
     while (true)
     {
         cout << "=====Programa de Encuestas=====\n1) Crear Cuenta"<<
@@ -22,14 +23,15 @@ int main(){
         << endl;
         cin >> Option; 
         Menu(Option);
-    }
-    
+    }   
 }
+
 void Menu(int Election){
     switch (Election){
         case 1:
             User Pollster = User(); // pollster = Encuestador
             char Name[255], FatherName[255], MotherName[255];
+            Pollster.SetID(IdCouter);
             cout << "Ingrese su nombre: ";
             scanf("%s", Name);
             cout << "Ingrese su Apellido Paterno: ";
@@ -37,6 +39,8 @@ void Menu(int Election){
             cout << "Ingrese su Apellido Materno: ";
             scanf("%s", MotherName);
             Pollster.SetFullName(Name, FatherName, MotherName);
+            cout << "Ingrese su Run (sin el digito verificador): ";
+            
             break;
         case 2:
             break;
@@ -44,6 +48,16 @@ void Menu(int Election){
             cout << "Bye bye!" << endl;
             exit(Election);
             break;
+    }
+}
+
+void IdVerifier(){
+    string AuxiliarString;
+    getline(UserFile, AuxiliarString);
+    if(IdCouter == NULL && AuxiliarString.length() == 0){
+        IdCouter = 1;
+    }else{
+        //Aqui seria el buscador en el archivo de texto
     }
 }
 /* Objetivos
